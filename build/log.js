4bad5d88241c83e78a84601ab28ff4853f49ff86
AmeyaJain-25 <ameiya.jain@gmail.com>
2025-02-02 00:13:38 +0530

    trial

--- build/log.js
+++ build/log.js
@@ -1,11 +1,41 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || (function () {
+    var ownKeys = function(o) {
+        ownKeys = Object.getOwnPropertyNames || function (o) {
+            var ar = [];
+            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
+            return ar;
+        };
+        return ownKeys(o);
+    };
+    return function (mod) {
+        if (mod && mod.__esModule) return mod;
+        var result = {};
+        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
+        __setModuleDefault(result, mod);
+        return result;
+    };
+})();
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.LogLevel = void 0;
 exports.logMessage = logMessage;
-var core_1 = __importDefault(require("@actions/core"));
+var core = __importStar(require("@actions/core"));
 var LogLevel;
 (function (LogLevel) {
     LogLevel["ERROR"] = "error";
@@ -20,29 +50,29 @@ function logMessage(level, message, options) {
     }; }
     switch (level) {
         case LogLevel.FAILED:
-            if (core_1.default.isDebug()) {
+            if (core.isDebug()) {
                 if (options.error instanceof Error || options.error !== undefined) {
-                    core_1.default.error(options.error);
+                    core.error(options.error);
                 }
             }
-            core_1.default.setFailed(message);
+            core.setFailed(message);
             break;
         case LogLevel.INFO:
-            core_1.default.info(message);
+            core.info(message);
             if (options.data !== undefined) {
-                core_1.default.info(JSON.stringify(options.data));
+                core.info(JSON.stringify(options.data));
             }
             break;
         case LogLevel.WARNING:
-            core_1.default.warning(message);
+            core.warning(message);
             break;
         default:
-            if (core_1.default.isDebug()) {
+            if (core.isDebug()) {
                 if (options.error instanceof Error || options.error !== undefined) {
-                    core_1.default.error(options.error);
+                    core.error(options.error);
                 }
             }
-            core_1.default.error(message);
+            core.error(message);
             break;
     }
 }
--- src/log.ts
+++ src/log.ts
@@ -1,4 +1,4 @@
-import core from "@actions/core";
+import * as core from "@actions/core";
 
 enum LogLevel {
   ERROR = "error", // Just log the message as an error and continue the execution
